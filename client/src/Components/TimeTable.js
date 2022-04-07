e97d627eb1c5ae9f7112a221566c36906310830a
matanNoach <matan.noach@gmail.com>
2022-04-07 23:49:47 +0300

    Implement table sort

--- client/src/Components/TimeTable.js
+++ client/src/Components/TimeTable.js
@@ -1,35 +1,129 @@
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
+import {
+    Paper,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    TableSortLabel,
+    Box,
+} from "@mui/material";
+import { visuallyHidden } from "@mui/utils";
 import React from "react";
 
+const headCells = [
+    {
+        title: "Date",
+        key: "date",
+    },
+    {
+        title: "Start",
+        key: "startTime",
+    },
+    {
+        title: "End",
+        key: "endTime",
+    },
+    {
+        title: "Total",
+        key: "totalString",
+    },
+    {
+        title: "Overtime",
+        key: "overtime",
+    },
+    {
+        title: "Payment",
+        key: "payment",
+    },
+];
+
+
+function descendingComparator(a, b, orderBy) {
+    if (b[orderBy] < a[orderBy]) {
+      return -1;
+    }
+    if (b[orderBy] > a[orderBy]) {
+      return 1;
+    }
+    return 0;
+  }
+  
+  function getComparator(order, orderBy) {
+    return order === 'desc'
+      ? (a, b) => descendingComparator(a, b, orderBy)
+      : (a, b) => -descendingComparator(a, b, orderBy);
+  }
+
 class TimeTable extends React.Component {
-    
+    constructor(props) {
+        super(props);
+        this.state = {
+            orderBy: "date",
+            order: "asc",
+        };
+    }
+    setOrder = (isAsc, property) => {
+        this.setState({
+            order: isAsc ? "desc" : "asc",
+            orderBy: property,
+        });
+    };
+    createSortHandler = (property) => (event) => {
+        this.handleRequestSort(event, property);
+    };
+    handleRequestSort = (event, property) => {
+        const isAsc = this.state.orderBy === property && this.state.order === "asc";
+        this.setOrder(isAsc, property);
+    };
     render() {
+        console.log(this.state)
         return (
             <TableContainer component={Paper}>
                 <Table>
                     <TableHead>
                         <TableRow>
-                            <TableCell>Date</TableCell>
-                            <TableCell>Start</TableCell>
-                            <TableCell>End</TableCell>
-                            <TableCell>Total</TableCell>
-                            <TableCell>Overtime</TableCell>
-                            <TableCell>Payment</TableCell>
+                            {headCells.map((head) => {
+                                console.log(head.key)
+                                return (
+                                    <TableCell
+                                        key={head.key}
+                                        sortDirection={this.state.orderBy === head.key ? this.state.order : false}
+                                    >
+                                        {head.title}
+                                        <TableSortLabel
+                                            active={this.state.orderBy === head.key}
+                                            direction={this.state.orderBy === head.key ? this.state.order : "asc"}
+                                            onClick={this.createSortHandler(head.key)}
+                                        >
+                                            {this.state.orderBy === head.key ? (
+                                                <Box component="span" sx={visuallyHidden}>
+                                                    {this.state.order === "desc"
+                                                        ? "sorted descending"
+                                                        : "sorted ascending"}
+                                                </Box>
+                                            ) : null}
+                                        </TableSortLabel>
+                                    </TableCell>
+                                );
+                            })}
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {this.props.data ? this.props.data.map((day, i) => {
-                            return (
-                                <TableRow key={i}>
-                                    <TableCell>{day.date}</TableCell>
-                                    <TableCell>{day.startTime}</TableCell>
-                                    <TableCell>{day.endTime}</TableCell>
-                                    <TableCell>{day.totalString}</TableCell>
-                                    <TableCell>{day.overtime}</TableCell>
-                                    <TableCell>{day.payment}</TableCell>
-                                </TableRow>
-                            );
-                        }): <></>}
+                        {this.props.data ? (
+                            this.props.data.sort(getComparator(this.state.order,this.state.orderBy)).map((day, i) => {
+                                return (
+                                    <TableRow key={i}>
+                                        {headCells.map((head) => {
+                                            return <TableCell>{day[head.key]}</TableCell>;
+                                        })}
+                                    </TableRow>
+                                );
+                            })
+                        ) : (
+                            <></>
+                        )}
                     </TableBody>
                 </Table>
             </TableContainer>
